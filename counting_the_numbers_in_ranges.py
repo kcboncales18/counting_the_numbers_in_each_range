202f31899190bbeb173cf6b5474309a7ab92ced0
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-31 20:29:31 +0800

    dictionary for the range of numbers

--- counting_the_numbers_in_ranges.py
+++ counting_the_numbers_in_ranges.py
@@ -1,1 +1,9 @@
 #counting_the_numbers_in_each_range
+
+#dictionary_for_the_range_of_numbers
+range_category= {"range_1":0,
+                 "range_2":0,
+                 "range_3":0,
+                 "range_4":0,
+                 "range_5":0
+                 }
