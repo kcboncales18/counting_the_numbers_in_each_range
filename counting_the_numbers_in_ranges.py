d74f194dab56baedb20a9ed442606fddecbd33b6
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-31 20:39:48 +0800

    ask the user to inputs numbers

--- counting_the_numbers_in_ranges.py
+++ counting_the_numbers_in_ranges.py
@@ -7,3 +7,12 @@ range_category= {"range_1":0,
                  "range_4":0,
                  "range_5":0
                  }
+
+#asking_the_user_to_input
+def asking_user():
+  try:
+    asking_user=int(input("Please,input a number: "))
+    return asking_user
+  except:
+    print("Error.")
+    return None
